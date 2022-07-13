cac2d9d1d40a81732d923e5fe7b851902f1718dc
Minh Nguyen <Minh.Nguyen@episerver.com>
2022-07-13 11:09:08 +0700

    Update scripts
    - Merge scripts for HAPI-1942
    - Add script for HAPI-2361
    - Regression test and update some scripts changed due to HAPI-1942 (new content type is add to site, which change the support media extension list)

--- CustomizedCode/Automation/Models/VariableItem.cs
+++ CustomizedCode/Automation/Models/VariableItem.cs
@@ -5,12 +5,5 @@ using System.Threading.Tasks;
 
 namespace AlloyTemplates.Automation.Models
 {
-    //public class VariableItem
-    //{
-    //    public string Key { get; set; }
-    //    public object Value { get; set; }
-    //    public bool Enable { get; set; } = true;
-    //}
     public record VariableItem(string Key, object Value, bool Enable = true);
-
 }
